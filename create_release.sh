eb51cc89f3707a411eadcc9444203db51371540f
striped-bass <github.chaste299@passinbox.com>
2026-07-18 15:01:48 -0700

    Fix file extension

--- create_release.sh
+++ create_release.sh
@@ -7,4 +7,4 @@ git tag ${date}
 cd icons/
 zip -q ../NieR_Cursors_Windows_${date}.zip nier_cursors_windows/*
 tar -caf ../NieR_XCursors_${date}.tar.xz nier_xcursors
-tar -caf ../NieR_Hyprcursors_${date}.zip nier_hyprcursors/*
+tar -caf ../NieR_Hyprcursors_${date}.tar.xz nier_hyprcursors/*
